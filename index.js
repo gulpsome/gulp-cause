9ca72a9b843d91e87bdd296546a386ae60c0bf6f
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-23 11:54:55 +0300

    gulp-cause can load config objects into maps

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ import R from 'ramda'
 import {gulpTask} from 'stamina'
 
 export function load (what) {
-  if (R.type(what) === 'Map') return what
-  else return new Map(what)
+  switch (R.type(what)) {
+    case 'Map':
+      return what
+    case 'Object':
+      return new Map(R.toPairs(what))
+    case 'Array':
+      return new Map(what)
+    default:
+      throw new Error(`Don't know how to convert \
+${what.toString()} of type ${R.type(what)} to Map.`)
+  }
 }
 
 export default function (gulp, causality) {
--- test/all.js
+++ test/all.js
@@ -3,7 +3,8 @@
 import {load} from '../index.js'
 
 describe('load', () => {
-  let expected = new Map([[1, 'one'], [2, 'two']])
+  let anObject = {one: 1, two: 2}
+  let expected = new Map([['one', 1], ['two', 2]])
 
   describe('a Map is returned as is', () => {
     it('given a Map', () => {
@@ -11,9 +12,15 @@ describe('load', () => {
     })
   })
 
+  describe('given a plain js Object', () => {
+    it('makes a Map', () => {
+      expect(load(anObject)).to.eql(expected)
+    })
+  })
+
   describe('an Array of Array pairs', () => {
     it('makes a Map', () => {
-      expect(load([[1, 'one'], [2, 'two']])).to.eql(expected)
+      expect(load([['one', 1], ['two', 2]])).to.eql(expected)
     })
   })
 
